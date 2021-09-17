417905da899e58a1ba3639b47b5c11cd983071af
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-09-17 10:50:17 -0700

    adding dotenv to server.js file

--- server.js
+++ server.js
@@ -15,9 +15,19 @@ app.use(express.json());
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: process.env.DB_USER,
+    host: 'localhost',
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
   },
   console.log(`Connected to the companyinfo_db database.`)
 );
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

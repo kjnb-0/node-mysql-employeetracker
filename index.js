451d10d7d1615e6dda0d4f9aae6868549f4d69ad
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-09-18 13:27:39 -0700

    add department function

--- index.js
+++ index.js
@@ -25,26 +25,25 @@ function mainQuestions() {
   inquirer.prompt ([
     {
       type: "list",
-      message: "What would you like to view?",
+      message: "What would you like to do?",
       name: "category",
-      choices: ["All Departments","All Roles","All Employees","None - I want to add"]
+      choices: ["View All Departments","View All Roles","View All Employees","Add Department","Add Role","Add Employee","Update Employee"]
     }
   ])
   .then(async (data) => {
     const { category } = data;
-    if (category === "All Departments") {
+    if (category === "View All Departments") {
       viewAllDepartments();
       return;
-    } else if (category === "All Roles") {
+    } else if (category === "View All Roles") {
       viewAllRoles();
       return;
-    } else if (category === "All Employees") {
+    } else if (category === "View All Employees") {
       viewAllEmployees();
       return;
-    } else {
-      // none - i want to add
-      //ask what we want to add,
-      //run functions for adding to each table 
+    } else if (category === "Add Department") {
+      addDepartment();
+      return;
     }
   })
 }
@@ -60,24 +59,37 @@ db.query('SELECT * FROM department', function (err, results) {
 }
 
 function viewAllRoles() {
-    // Query database
 db.query('SELECT * FROM role', function (err, results) {
   if(err) console.log(err)
   console.table(results)
-  //to go back to main menu
   mainQuestions();
 })
 }
 
-function viewAllEmployees(){
-    // Query database
+function viewAllEmployees() {
 db.query('SELECT * FROM employee', function (err, results) {
   if(err) console.log(err)
   console.table(results)
-  //to go back to main menu
   mainQuestions();
 })
 }
+
+function addDepartment() {
+  inquirer
+  .prompt({
+      type: 'input',
+      name: 'deptName',
+      message: 'Please enter the name of the department'
+  })
+  .then( answers => {
+       db.query(`INSERT INTO department SET ?`, {
+        name : answers.deptName
+       });
+        console.log("Added department")
+        mainQuestions();
+      });
+}
+
 
 //separate functions for view all depts, roles and employees then call them in the if statement
 mainQuestions();
